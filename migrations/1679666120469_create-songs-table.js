505dc8d83b494be5969941d798f3e7c30820d5f7
Kurniawan <kurnhyalcantara@gmail.com>
2023-03-24 22:15:23 +0800

    add db songs migrate file

---
+++ migrations/1679666120469_create-songs-table.js
@@ -0,0 +1,36 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      noteNull: true,
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    genre: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    duration: {
+      type: 'INTEGER',
+    },
+    albumId: {
+      type: 'TEXT',
+    },
+  });
+};
+
+exports.down = (pgm) => {};
